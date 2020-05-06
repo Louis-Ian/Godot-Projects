b4c9f0c34488e525887d0b309fef0d99bfdfd953
Louis Ian <louisiangt@gmail.com>
2020-05-06 04:46:04 -0300

    updated physics basic attributes, including smoother direction change using grip attribute

--- Formula Dots/Scripts/Car.gd
+++ Formula Dots/Scripts/Car.gd
@@ -1,18 +1,19 @@
 extends RigidBody2D
 
-var SCALE = 100
+var SCALE = 20
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var colors_array = [Color("ff0000"), Color("001aff"), Color("f7ff00"), Color("08ff00")]
+#var colors_array = [Color("ff0000"), Color("001aff"), Color("f7ff00"), Color("08ff00")]
 var screen_size
 
 var acceleration = 0	#acceleratino in meters per second squared
 var breaking = 0	#breaking in meters per second squared
 var speed = 0	#speed in meters per second
-var max_speed = 33*SCALE
-var direction = Vector2(0, 0)	#direction vetor
+var max_speed = 22*SCALE	#max speed in meters per second, corrected to the scale of the sprite
+var direction = Vector2(0, 0)	#direction vector
 var velocity = Vector2(0, 0)	#velocity vector
+var grip = 0.2	#grip level from 0 to 1
 
 
 func _init():
@@ -21,31 +22,38 @@ func _init():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
-	position = Vector2(300, 300)
+	position = Vector2(200, 200)
 	
 	randomize()
 	var new_color = Color(randf(), randf(), randf())
 	$"Outer Color".self_modulate = new_color
-	print(new_color)
-	print($"Outer Color".self_modulate)
 	
 	direction = Vector2(1, 0)
+	direction = direction.normalized()
 	acceleration = 10*SCALE
 	breaking = 50*SCALE
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
-	direction = Vector2(randf(), randf())
-
+	var random_x_sign = random_negative_or_positive_sign()
+	var random_y_sign = random_negative_or_positive_sign()
+	var new_direction = Vector2(direction.x + random_x_sign * randf() * grip, direction.y + random_y_sign * randf() * grip)
+	new_direction = new_direction.normalized()
+	#direction = lerp(direction, new_direction, grip)
+	direction = new_direction
+	print(direction)
+	
 	if speed != max_speed:
 		speed += acceleration * delta
 
-	velocity.x = direction.x * speed
-	velocity.y = direction.y * speed
-
-	position += velocity * delta
+	position += direction * speed * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
+#Function that generates random sign (positive or negative)
+func random_negative_or_positive_sign():
+	var random_sign = rand_range(-1, 1)
+	if random_sign < 0:
+		return -1.0
+	else:
+		return 1.0
