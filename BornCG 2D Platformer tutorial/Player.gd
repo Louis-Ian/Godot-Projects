eb6a846f1310220d42f49233503d369ea7fd66c7
Louis Ian <louisiangt@gmail.com>
2020-05-06 21:58:03 -0300

    Part 3
    - Gravity
    - Jump

--- BornCG 2D Platformer tutorial/Player.gd
+++ BornCG 2D Platformer tutorial/Player.gd
@@ -1,16 +1,21 @@
  extends KinematicBody2D
 
 var velocity = Vector2(0, 0)
+const SPEED = 180
+const GRAVITY = 25
+const JUMP_HEIGHT = -500
+const UP_DIRECTION = Vector2.UP
+
 #var indle_iteration = 0
 
 func _physics_process(delta):
 	
 	if Input.is_action_pressed("right"):
-		velocity = Vector2(120, 0)
+		velocity.x = SPEED
 #		print("GOING RIGHT")
 #		indle_iteration = 0
 	elif Input.is_action_pressed("left"):
-		velocity = Vector2(-120, 0)
+		velocity.x = -SPEED
 #		print("GOING LEFT")
 #		indle_iteration = 0
 #	elif(Input.is_action_pressed("ui_up")):
@@ -22,4 +27,12 @@ func _physics_process(delta):
 #		indle_iteration += 1
 #		print("INDLEING ", indle_iteration)
 	
-	move_and_slide(velocity)
+	if Input.is_action_just_pressed("up") and $".".is_on_floor():
+		velocity.y = JUMP_HEIGHT
+#		velocity.y = lerp(velocity.y, JUMP_HEIGHT, 0.34)
+	
+	velocity.y += GRAVITY
+	velocity = move_and_slide(velocity, UP_DIRECTION)
+	
+#	if $".".is_on_floor():
+#		velocity.y = 0
