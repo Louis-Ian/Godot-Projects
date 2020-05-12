079503757c64c707cf53bb8cbfc9e20e46dfce23
Louis Ian <louisiangt@gmail.com>
2020-05-12 09:35:23 -0300

    Animated sprite
    - various animations
    - ducking mechanic
    -

--- BornCG 2D Platformer tutorial/Player.gd
+++ BornCG 2D Platformer tutorial/Player.gd
@@ -6,30 +6,29 @@ const GRAVITY = 25
 const JUMP_HEIGHT = -500
 const UP_DIRECTION = Vector2.UP
 
-#var indle_iteration = 0
-
 func _physics_process(delta):
 	
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("down") and is_on_floor():
+		$AnimatedSprite.play("duck")
+		velocity.x = lerp(velocity.x, 0, 0.08)
+	elif Input.is_action_pressed("right"):
 		velocity.x = SPEED
-#		print("GOING RIGHT")
-#		indle_iteration = 0
+		if is_on_floor():
+			$AnimatedSprite.play("walking")
+			$AnimatedSprite.flip_h = false
 	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
-#		print("GOING LEFT")
-#		indle_iteration = 0
-#	elif(Input.is_action_pressed("ui_up")):
-#		velocity = Vector(-1, 0)
-#	elif(Input.is_action_pressed("ui_down")):
-#		velocity = Vector(-1, 0)
+		if is_on_floor():
+			$AnimatedSprite.play("walking")
+			$AnimatedSprite.flip_h = true
 	else:
 		velocity.x = lerp(velocity.x, 0, 0.2)
-#		indle_iteration += 1
-#		print("INDLEING ", indle_iteration)
+		if is_on_floor():
+			$AnimatedSprite.play("idle")
 	
-	if Input.is_action_just_pressed("up") and $".".is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_HEIGHT
-#		velocity.y = lerp(velocity.y, JUMP_HEIGHT, 0.34)
+		$AnimatedSprite.play("jumping")
 	
 	velocity.y += GRAVITY
 	velocity = move_and_slide(velocity, UP_DIRECTION)
